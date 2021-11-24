bbdab757416073965898011798c4ae0042f31a9d
hieptran <tranvanhiep15061998@gmail.com>
2021-11-24 09:45:41 +0700

    hihih

--- src/components/Admin/Brands/Brands.js
+++ src/components/Admin/Brands/Brands.js
@@ -26,7 +26,8 @@ function Brands() {
         Axios.get("http://localhost:8080/api/brand/get-all-brand").then((result) => {
             setBrands(result.data)
         });
-        window.scrollTo(0, 0)
+        window.scrollTo(0, 0);
+        console.log('brands sprint2');
     }, [check]);
 
     const openNew = (key) => {
